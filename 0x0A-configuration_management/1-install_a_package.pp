d89301fcaf36ca0548cd33282aee1af5734c1d44
St <takundaveni@gmail.com>
2024-04-19 18:13:26 -0700

    3?

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 #install flask using python
-exec {'flask_install':
-  command => 'pip3 install Flask==2.1.0',
-  path    =>'usr/local/bin',
-unless    => 'pip3 show Flask | grep Version | grep -q 2.1.0',
+package {'Flask':
+  ensure   => '2.1.0',
+  provider =>'pip3',
 }
