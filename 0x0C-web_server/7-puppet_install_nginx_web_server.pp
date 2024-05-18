88463b4f02f209eecdf7e7c9ff0da484f5f1fdf6
root <takundaveni@gmail.com>
2024-05-18 16:37:55 -0700

    tee

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,37 +1,30 @@
-# puppet code to install and configure nginx
-
-#check if nginx is installed
-package {'nginx':
-ensure => 'present',
+# Script that installs and configures Nginx
+exec {'update':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y update',
 }
 
-#install and update
-exec {'install_update':
-path     => '/usr/bin:/usr/sbin:/bin',
-command  => 'sudo apt-get update; sudo apt-get -y install nginx',
-provider => shell,
+exec {'install':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo apt-get -y install nginx',
 }
 
-#homepage
-exec {'homepage':
-command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
-provider => shell,
+exec {'echo_html':
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
+  command  => 'sudo echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html',
 }
 
-#redirect me page
-exec {'redirect_me':
-command  => 'echo "https://www.youtube.com/watch?v=QH2-TGUlwu4" | sudo tee /var/www/html/redirect_me',
-provider => shell,
+exec {'sed_config':
+  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
 }
 
-#configure nginx for redirect me
-exec {'nginx_config':
-provider => shell,
-command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
-}
-
-#start server
-exec {'start_server':
-command  => 'sudo service nginx start',
-provider => shell,
+exec {'start':
+  command  => 'sudo service nginx start',
+  provider => shell,
+  path     => '/usr/bin:/usr/sbin:/bin',
 }
