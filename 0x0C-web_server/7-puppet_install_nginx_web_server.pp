80d4bd8b2f526f2e8d96ff8e213b4b3d6042a7f2
root <takundaveni@gmail.com>
2024-05-18 10:29:29 -0700

    puppet test 1

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,36 @@
+# puppet code to install and configure nginx
+
+#check if nginx is installed
+package {'nginx':
+ensure => 'present',
+}
+
+#install and update
+exec {'install_update':
+command  => 'sudo apt-get update; sudo apt-get -y install nginx',
+provider => shell,
+}
+
+#homepage
+exec {'homepage':
+command  => 'echo "Hello World!" > var/www/html/index.html',
+provider => shell,
+}
+
+#redirect me page
+exec {'redirect_me':
+command  => 'echo "https://www.youtube.com/watch?v=QH2-TGUlwu4" > var/www/html/redirect_me',
+provider => shell,
+}
+
+#configure nginx for redirect me
+exec {'nginx_config':
+provider => shell,
+command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
+}
+
+#start server
+exec {'start_server':
+command  => 'sudo service nginx start',
+provider => shell,
+}
