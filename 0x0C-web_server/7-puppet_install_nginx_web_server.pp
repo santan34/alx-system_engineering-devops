3c6d882888de782cd4a48e0a381287b0a9c4db0e
root <takundaveni@gmail.com>
2024-05-18 18:03:39 -0700

    test 3

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -18,7 +18,7 @@ exec {'echo_html':
 }
 
 exec {'sed_config':
-  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me http://www.youtube.com permanent;" /etc/nginx/sites-available/default',
+  command  => 'sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4;" /etc/nginx/sites-available/default',
   provider => shell,
   path     => '/usr/bin:/usr/sbin:/bin',
 }
