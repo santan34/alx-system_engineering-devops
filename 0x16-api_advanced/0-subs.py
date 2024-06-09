253fbb651bb99d53a3e5d2a428e0cd358523bf17
St <takundaveni@gmail.com>
2024-06-09 14:16:19 +0200

    more zero test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""
-query the reddit api
-"""
 
-from requests import get
+"""Queries the Reddit API and returns the number of subscribers
+"""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """subredit"""
-    head = {'user-agent': 'Takudzwanashe ndaveni'}
-    response = get(
-        f"https://www.reddit.com/r/{subreddit}/about.json",
-        headers=head)
-    if response.status_code == 400:
-        return response.json().get("data").get("subscribers")
+    """Queries the Reddit API and returns the number of subscribers
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Test"}
+    req = requests.get(url, headers=headers)
+
+    if req.status_code == 200:
+        return (req.json().get("data").get("subscribers"))
     else:
         return (0)
