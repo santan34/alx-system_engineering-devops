bfffa570c34990a857904a0e36e3670cf9a631a4
St <takundaveni@gmail.com>
2024-06-09 14:06:19 +0200

    00test00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-query the reddit api
-"""
-
-from requests import get
+""" Exporting csv files"""
+import json
+import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """subredit"""
-    head = {'user-agent': 'Takudzwanashe ndaveni'}
-    response = get(
-        f"https://www.reddit.com/r/{subreddit}/about.json",
-        headers=head)
-    if response.status_code == 400:
-        return response.json().get("data").get("subscribers")
+    """Read reddit API and return number subscribers """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
     else:
-        return (0)
+        return(0)
