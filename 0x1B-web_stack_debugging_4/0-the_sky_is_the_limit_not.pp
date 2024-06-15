df4a6f91206e1fab1de6e99e9934d72eaa8a3fbf
St <takundaveni@gmail.com>
2024-06-15 16:47:50 +0200

    yo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,3 @@
-# puppet pupetter
-
-exec {'sets a file':
-  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
-  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
-  onlyif  => 'test -f /etc/default/nginx'
-}
+# Puppet script
+exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
+-> exec { '/usr/bin/env service nginx restart': }
